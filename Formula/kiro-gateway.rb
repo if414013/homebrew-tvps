fb39a759041d5349ddef30cc418b9e584b7f5ce2
Taufan Silitonga <taufansilitonga@gmail.com>
2026-01-27 17:02:52 +0700

    chore: bump kiro-gateway to 1.0.3

--- Formula/kiro-gateway.rb
+++ Formula/kiro-gateway.rb
@@ -1,28 +1,28 @@
 class KiroGateway < Formula
   desc "Proxy gateway for Kiro API - OpenAI and Anthropic compatible"
   homepage "https://github.com/if414013/rkgw"
-  version "1.0.2"
+  version "1.0.3"
   license "AGPL-3.0"
 
   on_macos do
     on_intel do
       url "https://github.com/if414013/rkgw/releases/download/v#{version}/kiro-gateway-x86_64-apple-darwin.tar.gz"
-      sha256 "a40a0fda496f6763e4203bd6def73841f24db379657b4ce0e0f31ae47b1ec14e"
+      sha256 "4ab45460afc792303928acee631aa5b92d7a3201520a0fc6a974958ea2eef36c"
     end
     on_arm do
       url "https://github.com/if414013/rkgw/releases/download/v#{version}/kiro-gateway-aarch64-apple-darwin.tar.gz"
-      sha256 "7774a1a60e5b58cce34be164b3c7c5d4d0915d241cb191ef5e1d484db490f728"
+      sha256 "8d73e416eef7fa8c17f0a30cec903f58645749b9ab9fd40b4a7b2f1f2e439754"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/if414013/rkgw/releases/download/v#{version}/kiro-gateway-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9bff94cb0e0b86184d17f5fce4840827b9f421ef5ad1169afea34b8314d800fd"
+      sha256 "7405aa04438bef37b7100104e3136082d7b300153f9a4552c106c66e36c39c04"
     end
     on_arm do
       url "https://github.com/if414013/rkgw/releases/download/v#{version}/kiro-gateway-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "932d175d5acfbc159732b408a684eaf316f9dacc6bca678705ae0849bfe685b5"
+      sha256 "f25db05a31ca8593d30b703d9311a66917d9ff29ec52c8f075913267b03817ee"
     end
   end
 
